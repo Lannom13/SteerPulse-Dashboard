4ba0fdc93c327f446fe79603c22833fc9e88273f
Lannom13 <anlannom42@gmail.com>
2025-06-19 16:17:52 -0500

    Update Budgeting.jsx

--- src/pages/Budgeting.jsx
+++ src/pages/Budgeting.jsx
@@ -1,3 +1,11 @@
+import AnimatedPage from '../components/AnimatedPage'
+
 export default function Budgeting() {
-  return <div className="text-white">📊 Budgeting Section Coming Soon</div>
+  return (
+    <AnimatedPage>
+      <div className="text-white">
+        📊 Budgeting Section Coming Soon
+      </div>
+    </AnimatedPage>
+  )
 }
