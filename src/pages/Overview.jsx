1b6925dd44709ebe03f36959fde6fb6e0fded9af
Lannom13 <anlannom42@gmail.com>
2025-06-19 16:17:07 -0500

    Update Overview.jsx

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -1,8 +1,12 @@
 // src/pages/Overview.jsx
+import AnimatedPage from '../components/AnimatedPage'
+
 export default function Overview() {
   return (
-    <div className="text-white">
-      🧭 <strong>SteerPulse Overview</strong> – At-a-glance dashboard coming soon!
-    </div>
+    <AnimatedPage>
+      <div className="text-white">
+        🧭 <strong>SteerPulse Overview</strong> – At-a-glance dashboard coming soon!
+      </div>
+    </AnimatedPage>
   )
 }
