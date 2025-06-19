8ac36e68c588cd4d217f7401ae542098fa94cb37
Lannom13 <anlannom42@gmail.com>
2025-06-19 16:19:31 -0500

    Update Settings.jsx

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,3 +1,11 @@
+import AnimatedPage from '../components/AnimatedPage'
+
 export default function Settings() {
-  return <div className="text-white">⚙️ Settings Section Coming Soon</div>
+  return (
+    <AnimatedPage>
+      <div className="text-white">
+        ⚙️ Settings Section Coming Soon
+      </div>
+    </AnimatedPage>
+  )
 }
