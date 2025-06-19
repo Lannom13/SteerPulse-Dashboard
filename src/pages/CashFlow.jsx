a07742a0aa8184da050e7ab827109cd9a9542646
Lannom13 <anlannom42@gmail.com>
2025-06-19 16:18:01 -0500

    Update CashFlow.jsx

--- src/pages/CashFlow.jsx
+++ src/pages/CashFlow.jsx
@@ -1,3 +1,11 @@
+import AnimatedPage from '../components/AnimatedPage'
+
 export default function CashFlow() {
-  return <div className="text-white">💸 Cash Flow Section Coming Soon</div>
+  return (
+    <AnimatedPage>
+      <div className="text-white">
+        💸 Cash Flow Section Coming Soon
+      </div>
+    </AnimatedPage>
+  )
 }
