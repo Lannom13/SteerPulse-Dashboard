c1ffe5415591771b72239d4f29f36768978e5140
Lannom13 <anlannom42@gmail.com>
2025-06-19 15:58:42 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import Sidebar from './components/Sidebar'
 import DashboardLayout from './layouts/DashboardLayout'
 
+// Import all page components
+import Overview from './pages/Overview'
 import Budgeting from './pages/Budgeting'
 import CashFlow from './pages/CashFlow'
 import Debt from './pages/Debt'
 import Transactions from './pages/Transactions'
 import Recurring from './pages/Recurring'
+import Investments from './pages/Investments'
 import Settings from './pages/Settings'
-import Insights from './pages/Insights'
 
 function App() {
   return (
@@ -18,14 +20,15 @@ function App() {
         <Sidebar />
         <DashboardLayout>
           <Routes>
-            <Route path="/" element={<Navigate to="/budgeting" />} />
+            <Route path="/" element={<Navigate to="/overview" />} />
+            <Route path="/overview" element={<Overview />} />
             <Route path="/budgeting" element={<Budgeting />} />
             <Route path="/cashflow" element={<CashFlow />} />
             <Route path="/debt" element={<Debt />} />
             <Route path="/transactions" element={<Transactions />} />
             <Route path="/recurring" element={<Recurring />} />
+            <Route path="/investments" element={<Investments />} />
             <Route path="/settings" element={<Settings />} />
-            <Route path="/insights" element={<Insights />} />
           </Routes>
         </DashboardLayout>
       </div>
