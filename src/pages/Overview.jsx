86dad75e8673ee960595132498eed5709b10817d
Lannom13 <anlannom42@gmail.com>
2025-06-19 15:57:55 -0500

    Create Overview.jsx

---
+++ src/pages/Overview.jsx
@@ -0,0 +1,8 @@
+// src/pages/Overview.jsx
+export default function Overview() {
+  return (
+    <div className="text-white">
+      🧭 <strong>SteerPulse Overview</strong> – At-a-glance dashboard coming soon!
+    </div>
+  )
+}
