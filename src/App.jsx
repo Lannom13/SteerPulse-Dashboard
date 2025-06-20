438bf1f7cc4c3e7f0d3c02441d499ecb8d8056e1
Lannom13 <anlannom42@gmail.com>
2025-06-20 08:22:12 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-// src/App.jsx
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Sidebar from './components/Sidebar'
 import DashboardLayout from './layouts/DashboardLayout'
 
-// Import all page components
+// Pages
 import Overview from './pages/Overview'
 import Budgeting from './pages/Budgeting'
-import CashFlow from './pages/CashFlow'
+import BudgetSpreadsheet from './pages/BudgetSpreadsheet'
+import Investments from './pages/Investments'
 import Debt from './pages/Debt'
 import Transactions from './pages/Transactions'
 import Recurring from './pages/Recurring'
-import Investments from './pages/Investments'
 import Goals from './pages/Goals'
 import Insights from './pages/Insights'
 import Notifications from './pages/Notifications'
@@ -26,11 +25,11 @@ function App() {
             <Route path="/" element={<Navigate to="/overview" />} />
             <Route path="/overview" element={<Overview />} />
             <Route path="/budgeting" element={<Budgeting />} />
-            <Route path="/cashflow" element={<CashFlow />} />
+            <Route path="/budgeting/spreadsheet" element={<BudgetSpreadsheet />} />
+            <Route path="/investments" element={<Investments />} />
             <Route path="/debt" element={<Debt />} />
             <Route path="/transactions" element={<Transactions />} />
             <Route path="/recurring" element={<Recurring />} />
-            <Route path="/investments" element={<Investments />} />
             <Route path="/goals" element={<Goals />} />
             <Route path="/insights" element={<Insights />} />
             <Route path="/notifications" element={<Notifications />} />
