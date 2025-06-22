e04ede2b5654eb707cd51f6f5c3c6846f1883484
Lannom13 <anlannom42@gmail.com>
2025-06-22 13:47:22 -0500

    Update supabaseClient.js

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -2,6 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://lpczocldblkfrhnlpqgf.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'; // ← use your actual anon key here
+const supabaseAnonKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxwY3pvY2xkYmxrZnJobmxwcWdmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAzNjQ2MTYsImV4cCI6MjA2NTk0MDYxNn0.OEtG4Kbk7r_X2M7OKgOsGbkd8WDi-IhhyusuWqUgsoY';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
