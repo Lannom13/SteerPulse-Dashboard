22815c40b0e924bb3e810c634177f3b15278f1d5
Lannom13 <anlannom42@gmail.com>
2025-06-24 21:37:41 -0500

    Update Transactions.jsx

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -1,11 +1,15 @@
-import AnimatedPage from '../components/AnimatedPage'
+// File: /pages/transactions.tsx
+import AnimatedPage from '@/components/AnimatedPage';
+import { NextPage } from 'next';
 
-export default function Transactions() {
+const Transactions: NextPage = () => {
   return (
     <AnimatedPage>
       <div className="text-white">
         🔁 Transactions Section Coming Soon
       </div>
     </AnimatedPage>
-  )
-}
+  );
+};
+
+export default Transactions;
