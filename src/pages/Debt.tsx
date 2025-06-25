d6ddf4fa35760daeb1d28b300904c38877f91867
Lannom13 <anlannom42@gmail.com>
2025-06-24 21:32:30 -0500

    Update and rename Debt.jsx to Debt.tsx

---
+++ src/pages/Debt.tsx
@@ -0,0 +1,15 @@
+// File: /pages/debt.tsx
+import AnimatedPage from '@/components/AnimatedPage';
+import { NextPage } from 'next';
+
+const Debt: NextPage = () => {
+  return (
+    <AnimatedPage>
+      <div className="text-white">
+        📉 Debt Section Coming Soon
+      </div>
+    </AnimatedPage>
+  );
+};
+
+export default Debt;
