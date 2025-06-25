9b5ecd9b95ee091193a1b5aced4c1a3f0b4a60a3
Lannom13 <anlannom42@gmail.com>
2025-06-24 21:37:02 -0500

    Update and rename Settings.jsx to Settings.tsx

---
+++ src/pages/Settings.tsx
@@ -0,0 +1,15 @@
+// File: /pages/settings.tsx
+import AnimatedPage from '@/components/AnimatedPage';
+import { NextPage } from 'next';
+
+const Settings: NextPage = () => {
+  return (
+    <AnimatedPage>
+      <div className="text-white">
+        ⚙️ Settings Section Coming Soon
+      </div>
+    </AnimatedPage>
+  );
+};
+
+export default Settings;
